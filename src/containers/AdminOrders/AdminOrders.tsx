feba613d1e7ea4adc2aca0a9e410bfdbb4454b98
Azamat Murzaliev <lirog.kg@gmail.com>
2023-01-07 10:19:18 +0600

    Немного поправил верстку

--- src/containers/AdminOrders/AdminOrders.tsx
+++ src/containers/AdminOrders/AdminOrders.tsx
@@ -3,6 +3,7 @@ import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {selectDishes, selectFetchAllOrdersLoading, selectOrders} from "../../store/HomeSlice";
 import {completeOrder, fetchDishes, fetchOrders} from "../../store/HomeThunks";
 import Spinner from "../../components/Spinner/Spinner";
+import {DELIVERY_PRICE} from "../../constants";
 
 const AdminOrders = () => {
     const dispatch = useAppDispatch();
@@ -34,13 +35,16 @@ const AdminOrders = () => {
                         const dish = dishes.find(dish => dish.id === id);
 
                         if (dish) {
+                            const t = () => {
+                                return dish.price * order.dishes[id]
+                            }
                             dishesInfo.push(
-                                <div className="d-flex align-items-center justify-content-between mt-5" key={dish.id}>
+                                <div className="d-flex align-items-center justify-content-between m-0" key={dish.id}>
                                     <div className="d-flex align-items-center justify-content-between">
                                         <p className="me-3 m-0">{dish.name}</p>
                                         <p className="m-0">x {order.dishes[id]}</p>
                                     </div>
-                                    <div className="d-flex align-items-center justify-content-between">
+                                    <div>
                                         <p className="m-0 me-2">{dish.price * order.dishes[id]} KGS</p>
                                     </div>
                                 </div>
@@ -48,8 +52,16 @@ const AdminOrders = () => {
                         }
                     }
 
-                    return <div key={order.id} className={"border"}>
+                    return <div key={order.id} className="border p-2 mb-3">
                         {dishesInfo}
+                        <div className="d-flex align-items-center justify-content-between m-0">
+                            <p>Delivery</p>
+                            <p className="me-2">{DELIVERY_PRICE} KGS</p>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between m-0 border-top">
+                            <p>Order Total:</p>
+                            <p className="me-2"> KGS</p>
+                        </div>
                         <button className="btn btn-danger" onClick={() => completeOrderById(order.id)}>Complete</button>
                     </div>
                 })}
--- src/containers/ClientCart/ClientCart.tsx
+++ src/containers/ClientCart/ClientCart.tsx
@@ -29,7 +29,7 @@ const ClientCart = () => {
 
       if (dish) {
           dishesInfo.push(
-              <div className="d-flex align-items-center justify-content-between mt-5" key={dish.id}>
+              <div className="d-flex align-items-center justify-content-between mt-1" key={dish.id}>
                   <div className="d-flex align-items-center justify-content-between">
                       <p className="me-3 m-0">{dish.name}</p>
                       <p className="m-0">x {order.dishes[id]}</p>
